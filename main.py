6c07444d696e7ba9b68fa4c8033233d7e8717eca
7과11사이 <dongjunlee222@gmail.com>
2023-11-27 18:36:57 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import feedparser, time
+
+URL="[https://v2.velog.io/rss/jacks222]"
+RSS_FEED = feedparser.parse(URL)
+MAX_POST=7
+
+markdown_text = """
+[정리 중인 기술 및 회고 내용 🚀]
+""" # list of blog posts will be appended here
+
+
+for idx, feed in enumerate(RSS_FEED['entries']):
+    if idx > MAX_POST:
+        break
+    else:
+        feed_date = feed['published_parsed']
+        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+
+f = open("README.md",mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
